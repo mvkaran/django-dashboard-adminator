d768d665d3006fecb40c284a7654e511cc72f1f8
MV Karan <mv@karan.be>
2020-07-26 23:50:03 +0530

    Added TF, modified Actions & Docker Compose

---
+++ aws-terraform/spinup.tf
@@ -0,0 +1,16 @@
+/*
+16.04: ami-2757f631
+16.10: ami-b374d5a5
+
+*/
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "webserver" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+  key_name = "TerraformKeyPair"
+}
